5426e71248ad7bc056b58e737e0f81cf59b3c300
Dmitry Arkhipov <grisumbras@gmail.com>
2018-12-10 23:47:52 +0300

    implemented passing options to b2

--- conanfile.py
+++ conanfile.py
@@ -4,9 +4,11 @@ from conans import (
 )
 from conans.client import join_arguments
 from conans.util.files import mkdir
+import collections
 import functools
 import itertools
 import os
+import six
 
 
 class B2ToolConan(ConanFile):
@@ -46,6 +48,91 @@ class folder(object):
         delattr(instance, "_" + self.name)
 
 
+class OptionsProxy(object):
+    _special_options = {
+        "--project-config": "project_config",
+        "--build-dir": "build_folder",
+    }
+
+    def __init__(self, b2):
+        setter = super(OptionsProxy, self).__setattr__
+        setter("_b2", b2)
+        setter("_conanfile", getattr(b2, "_conanfile"))
+        setter("_values", {})
+
+    def __getitem__(self, key):
+        key = self._b2ify(key)
+        return self._values[key]
+
+    def __setitem__(self, key, value):
+        key = self._b2ify(key)
+
+        if key in self._special_options.keys():
+            setattr(self._b2, self._special_options[key], value)
+        else:
+            self._values[key] = value
+
+    def __delitem__(self, key):
+        key = self._b2ify(key)
+        del self._values[key]
+
+    def __getattr__(self, key):
+        return self[key]
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, key):
+        del self[key]
+
+    def __call__(self, *args, **kw):
+        # pylint: disable=dict-items-not-iterating
+        for k, v in itertools.chain([(a, True) for a in args], kw.items()):
+            self[k] = v
+
+    def items(self):
+        return self._values.items()
+
+    def dir(self):
+        return [
+            opt.lstrip("-").replace("-", "_") for opt in self._values.keys()
+        ]
+
+    def strings(self):
+        return (self._stringify(k, v) for (k, v) in self.items())
+
+    def _b2ify(self, key):
+        if key.startswith('-'):
+            return key
+
+        key = key.replace("_", "-")
+        if len(key) == 1:
+            key = "-" + key
+        else:
+            key = "--" + key
+        return key
+
+    def _stringify(self, option, value):
+        if value is True:
+            return option
+
+        if (
+            not isinstance(value, six.string_types)
+            and isinstance(value, collections.Iterable)
+        ):
+            return "{option} {values}".format(
+                option=option,
+                values=" ".join([str(v) for v in value]),
+            )
+        elif option[1] != '-':
+            return option + str(value)
+        else:
+            return "{option}={value}".format(
+                option=option,
+                value=value,
+            )
+
+
 class B2(object):
     def __init__(self, conanfile):
         """
@@ -54,6 +141,13 @@ class B2(object):
 
         self._conanfile = conanfile
         self._settings = conanfile.settings
+
+        self.options = OptionsProxy(self)
+        self.options(
+            "hash",
+            j=tools.cpu_count(),
+            d=tools.get_env("CONAN_B2_DEBUG", "1"),
+        )
 
     @folder
     def source_folder(self): pass
@@ -68,41 +162,40 @@ class B2(object):
     def project_config(self):
         return os.path.join(self.build_folder, "project-config.jam")
 
-    def configure(self, requirements=None, options=None, **kw_options):
+    def configure(self, requirements=None):
         if not self._conanfile.should_configure:
             return
-
-        kw_options.update(options or dict())
 
         mkdir(self.build_folder)
         with open(self.project_config, "w") as config_file:
             self._write_toolchain(config_file)
-            self._write_options(config_file, kw_options)
             self._write_project(config_file, requirements or [])
 
-    def build(self, *targets, args=None):
-        if not self._conanfile.should_build:
+    def build(self, *targets):
+        if not (targets or self._conanfile.should_build):
             return
-        self._build(args or [], targets)
+        self._build(targets)
 
-    def install(self, args=None):
+    def install(self):
         if not self._conanfile.should_install:
             return
-        self._build(args or [], ["install"])
+        self._build(["install"])
 
-    def test(self, args=None):
+    def test(self):
         if not self._conanfile.should_test:
             return
-        self._build(args or [], ["test"])
+        self._build(["test"])
 
-    def _build(self, args, targets):
-        options = [
+    def _build(self, targets):
+        special_options = (
             "--project-config=" + self.project_config,
-            "-j%s" % tools.cpu_count(),
-            "-d+%s" % tools.get_env("CONAN_B2_DEBUG", "1"),
-            "--hash",
-        ]
-        args = itertools.chain(options, args, targets)
+            "--build-dir=" + self.build_folder,
+        )
+        args = itertools.chain(
+            special_options,
+            self.options.strings(),
+            targets,
+        )
         with tools.chdir(self.source_folder):
             b2_command = "b2 " + join_arguments(args)
             self._conanfile.output.info("%s" % b2_command)
@@ -111,19 +204,8 @@ class B2(object):
     def _write_toolchain(self, config_file):
         config_file.write("using gcc ;\n\n")
 
-    def _write_options(self, config_file, options):
-        config_file.write("import option ;\n")
-        for k, v in options.items():
-            if v is not None:
-                pattern = "option.set {k} : {v} ;\n"
-            else:
-                pattern = "option.set {k} ;\n"
-            config_file.write(pattern.format(k=k, v=v))
-        config_file.write("\n")
-
     def _write_project(self, config_file, requirements):
         config_file.write("project\n")
-        config_file.write("  : build-dir {0}\n".format(self.build_folder))
         config_file.write("  : requirements\n")
         requirements = itertools.chain(
             requirements,
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -12,13 +12,15 @@ build_helper = python_requires("b2-helper/0.0.1@grisumbras/testing")
 class B2ToolTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "b2"
-    build_requires = "boost_build/1.67.0@bincrafters/testing"
+    build_requires = "boost_build/1.68.0@bincrafters/testing"
 
     def build(self):
         builder = build_helper.B2(self)
         builder.source_folder = "src"
         builder.build_folder = "build"
-        builder.configure(foo=None, bar="baz")
+        builder.options.foo = True
+        builder.options(bar="baz")
+        builder.configure()
         builder.build()
 
     def test(self):
