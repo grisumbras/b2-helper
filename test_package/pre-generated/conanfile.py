44a22bd2562e62ce5afee5bd074cbdda6128801d
Dmitry Arkhipov <grisumbras@gmail.com>
2019-09-20 10:11:32 +0300

    adding Windows to CI

--- test_package/pre-generated/conanfile.py
+++ test_package/pre-generated/conanfile.py
@@ -4,7 +4,10 @@
 # file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 
-from conans import ConanFile
+from conans import (
+    ConanFile,
+    tools,
+)
 from get_helper_package import b2
 import os
 
@@ -13,14 +16,32 @@ import os
 class MyConan(ConanFile):
     """This package bootstraps its own project-config.jam"""
 
-    exports_sources = "*.sh", "*.jam", "*.cpp"
+    exports_sources = "*.bat", "*.sh", "*.jam", "*.cpp"
+
+    def build(self):
+        try:
+            super(MyConan, self).build()
+        except Exception as exc:
+            self.output.info(tools.load(self._bootstrap_script))
+            self.output.info(tools.load(
+                os.path.join(self.build_folder, "project-config.jam")
+            ))
+            self.output.info(tools.load(
+                os.path.join(self.build_folder, "helper.jam")
+            ))
+            raise
 
     def b2_setup_builder(self, builder):
         builder.include.append(self._bootstrap())
         builder.project_config = os.path.join(self.build_folder, "helper.jam")
         return builder
 
+    @property
+    def _bootstrap_script(self):
+        script = "bootstrap." + ("bat" if tools.os_info.is_windows else "sh")
+        return os.path.join(self.source_folder, script)
+
     def _bootstrap(self):
         if self.should_configure:
-            self.run(os.path.join(self.source_folder, "bootstrap.sh"))
+            self.run(self._bootstrap_script)
         return os.path.join(self.build_folder, "project-config.jam")
