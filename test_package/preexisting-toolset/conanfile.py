78d03af8b065535dd491605bd6039c892061cb7f
Dmitry Arkhipov <grisumbras@gmail.com>
2019-10-10 09:02:36 +0300

    only load toolsets if they aren't registered

---
+++ test_package/preexisting-toolset/conanfile.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2019 Dmitry Arkhipov <grisumbras@gmail.com>
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+
+import os
+from conans import ConanFile
+from get_helper_package import b2
+
+
+@b2.build_with_b2
+class MyConan(ConanFile):
+    exports_sources = "*.jam"
+
+    def b2_setup_builder(self, builder):
+        builder.using(("foobar", "1"))
+        builder.options.user_config = os.path.join(
+            self.source_folder, "user-config.jam",
+        )
+        return builder
+
+    def package(self):
+        pass
+
